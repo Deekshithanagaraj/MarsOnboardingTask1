c07dae5b5c90f457433d5e12061e85a0279a16d9
Deekshithanagaraj <deekshithangowda06@gmail.com>
2024-02-21 21:48:39 +0800

    This is to commit the language step definition for the mars application

---
+++ MarsOnboardingTask1/StepDefinitions/LanguageStepDefinition.cs
@@ -0,0 +1,100 @@
+﻿using MarsOnboardingTask1.Utilities;
+using NUnit.Framework;
+using MarsOnboardingTask1.Pages;
+using OpenQA.Selenium.Chrome;
+
+namespace MarsOnboardingTask1.StepDefinitions
+{
+
+    [Binding]
+    public class LanguageStepDefinitions : CommonDriver
+    {
+        Language LanguageObj;
+        LoginPage LoginPageObj;
+
+        [Given(@"I signed in the portal")]
+        public void GivenISignedInThePortal()
+        {
+            //Open Chrome browser
+            driver = new ChromeDriver();
+
+            //Initiate objects
+            LanguageObj = new Language();
+            LoginPageObj = new LoginPage();
+
+            //signin
+            LoginPageObj.LogInActions();
+        }
+
+        [When(@"I click on Languages tab")]
+        public void WhenIClickOnTabLanguages()
+        {
+            LanguageObj.ClickAnyTab("Languages");
+        }
+
+        [When(@"I add a '([^']*)' at a '([^']*)'")]
+        public void WhenIAddLanguage(string language, string languageLevel)
+        {
+            LanguageObj.AddALanguage(language, languageLevel);
+
+        }
+
+        [Then(@"The '([^']*)' with '([^']*)' should be added successfully")]
+        public void ThenTheLanguageShouldBeAddedSuccessfully(string language, string languageLevel)
+        {
+            //Check success message
+            string message = LanguageObj.GetMessage();
+            string assertMessage = language + " has been added to your languages";
+            Assert.That(message == assertMessage, "Actual message and Expected message do not match");
+
+            //check language and language level are created successfully
+            string addedLanguage = LanguageObj.GetLanguage();
+            string addedLanguageLevel = LanguageObj.GetLanguageLevel();
+            Assert.That(addedLanguage == language, "Actual language and Expected language do not match");
+            Assert.That(addedLanguageLevel == languageLevel, "Actual language level and Expected language level do not match");
+            driver.Close();
+        }
+
+        [When(@"I edit the last '([^']*)' at a different '([^']*)'")]
+        public void WhenIEditTheLastLanguage(string language, string languageLevel)
+        {
+            LanguageObj.EditLanguage(language, languageLevel);
+        }
+
+        [Then(@"The '([^']*)' and '([^']*)' should be edited successfully")]
+        public void ThenTheLanguageShouldBeEditedSuccessfully(string language, string languageLevel)
+        {
+            //Check message
+            string message = LanguageObj.GetMessage();
+            string assertMessage = language + " has been updated to your languages";
+            Assert.That(message == assertMessage, "Actual message and Expected message do not match");
+
+            //check if language and language level are updated successfully
+            string editedLanguage = LanguageObj.GetLanguage();
+            string editedLanguageLevel = LanguageObj.GetLanguageLevel();
+            Assert.That(editedLanguage == language, "Actual language and Expected language do not match");
+            Assert.That(editedLanguageLevel == languageLevel, "Actual language level and Expected language level do not match");
+            driver.Close();
+        }
+
+        [When(@"I delete the last '([^']*)'")]
+        public void WhenIDeleteTheLastLanguage(string language)
+        {
+            LanguageObj.DeleteLanguage(language);
+        }
+
+        [Then(@"The '([^']*)' should be deleted successfully")]
+        public void ThenTheLanguageShouldBeDeletedSuccessfully(string language)
+        {
+            //Check message
+            string message = LanguageObj.GetMessage();
+            string assertMessage = language + " has been deleted from your languages";
+            Assert.That(message == assertMessage, "Actual message and Expected message do not match.");
+
+            //check if language is deleted successfully
+            string lastLanguage = LanguageObj.GetLanguage();
+            Assert.That(lastLanguage != language, "Expected language has not been deleted.");
+            driver.Close();
+        }
+    }
+}
