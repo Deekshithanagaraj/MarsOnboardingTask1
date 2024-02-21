33850007e3753e203900349fdeead277ad6d5ce1
Deekshithanagaraj <deekshithangowda06@gmail.com>
2024-02-21 22:17:14 +0800

    This commit is to skills functionality of the mars application

---
+++ MarsOnboardingTask1/Hooks/Hooks1.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace MarsOnboardingTask1.Hooks
+{
+    [Binding]
+    public sealed class Hooks1
+    {
+        private ChromeDriver driver;
+
+        [BeforeScenario]
+        public void BeforeScenarioWithTag()
+        {
+
+            driver = new ChromeDriver();
+
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+
+            driver.Close();
+        }
+    }
+}
